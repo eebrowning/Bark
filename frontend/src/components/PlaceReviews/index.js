51ffda55cd79a6aa5cc030fc53b5252012b841ac
eebrowning <eebrowning828@gmail.com>
2022-06-30 17:20:17 -0600

    review CR, working on D

--- frontend/src/components/PlaceReviews/index.js
+++ frontend/src/components/PlaceReviews/index.js
@@ -2,7 +2,7 @@ import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { useHistory, useParams } from "react-router-dom"
 import { thunkGetPlace } from "../../store/place"
-import { thunkCreateReview, thunkGetAllReviews } from '../../store/review'
+import { thunkCreateReview, thunkDeleteReview, thunkGetAllReviews } from '../../store/review'
 
 
 const PlaceReviews = () => {
@@ -10,6 +10,8 @@ const PlaceReviews = () => {
     // console.log(placeId, 'placeId in PlaceReviews')
     const history = useHistory();
     const dispatch = useDispatch();
+    const sessionState = useSelector((state) => state.session);
+    const loggedInUserId = sessionState.user.id;
 
     const reviewsArray = useSelector(state => Object.values(state.reviewsState))
     // console.log(reviewsArray, "reviews state array, PlaceReviews");
@@ -26,17 +28,33 @@ const PlaceReviews = () => {
         history.push(`/places/${placeId}/review`);
         // dispatch(thunkUpdatePlace(place)); NO! dispatch will go on submit of edit form!
     }
+    const handleDelete = (e) => {
+        e.preventDefault();
+        const reviewId = e.target.id.split('-')[1]
+        dispatch(thunkDeleteReview(reviewId));///need to get reviewId
 
+        history.push(`/places/${placeId}`)
+    }
     return (
         <span id="reviews-span">
             <h1>Reviews:</h1>
             {reviewsArray?.map((review) => {
                 if (review.placeId == placeId) {
                     return (
-                        <span key={review.id} id={`place-box-${review.id}`} >
-                            <img height={'100px'} width={'100px'} src={review.imageURL} alt="alt"></img>
-                            <h2 id={`place-${review.id}`} onClick={handleClick}>{review.title}</h2>
-                        </span>
+                        <>
+                            <span key={review.id} id={`place-box-${review.id}`} >
+                                <img height={'100px'} width={'100px'} src={review.imageURL} alt="alt"></img>
+                                <h2 id={`place-${review.id}`} >{review.title}</h2>
+                            </span>
+                            {loggedInUserId === review.userId && (
+                                <>
+                                    {/* <button id='edit-place' onClick={handleEdit}>Edit</button> */}
+                                    <button id={`delete-${review.reviewId}`} onClick={handleDelete}>Delete</button>
+                                </>
+                            )}
+
+                        </>
+
                     )
                 }
             })}
